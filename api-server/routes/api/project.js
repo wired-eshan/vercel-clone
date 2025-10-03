926e9b0582e1abb8b0ea9dc912d4610eaed800ba
wired-eshan <eshanjan3@gmail.com>
2025-10-03 20:11:13 +0530

    Delete project build files from AWS S3

--- api-server/routes/api/project.js
+++ api-server/routes/api/project.js
@@ -1,14 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { authMiddleware } = require('../../middlewares/auth');
-const { generateSlug } = require('random-word-slugs');
-const { ECSClient, RunTaskCommand } = require('@aws-sdk/client-ecs');
-const { z } = require('zod');
-const { PrismaClient } = require('../../generated/prisma');
+const { authMiddleware } = require("../../middlewares/auth");
+const { generateSlug } = require("random-word-slugs");
+const { ECSClient, RunTaskCommand } = require("@aws-sdk/client-ecs");
+const { z } = require("zod");
+const { PrismaClient } = require("../../generated/prisma");
 const { createClient } = require("@clickhouse/client");
+const {
+  S3Client,
+  ListObjectsV2Command,
+  DeleteObjectsCommand,
+} = require("@aws-sdk/client-s3");
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 router.use(authMiddleware);
@@ -16,17 +21,25 @@ router.use(authMiddleware);
 const prisma = new PrismaClient();
 
 const ecsClient = new ECSClient({
-    region: 'ap-south-1',
-    credentials: {
-        accessKeyId: process.env.AWS_ECS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_ECS_SECRET_ACCESS_KEY,        
-    }
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ECS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_ECS_SECRET_ACCESS_KEY,
+  },
 });
 
 const ecsConfig = {
-    cluster: process.env.AWS_ECS_CLUSTER,
-    task: process.env.AWS_ECS_TASK
+  cluster: process.env.AWS_ECS_CLUSTER,
+  task: process.env.AWS_ECS_TASK,
 };
+
+const s3Client = new S3Client({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.AWS_S3_ACCESSKEYID,
+    secretAccessKey: process.env.AWS_S3_ACCESSKEY,
+  },
+});
 
 const client = createClient({
   host: process.env.CLICKHOUSE_HOST,
@@ -35,208 +48,252 @@ const client = createClient({
   database: "default",
 });
 
-router.post('/create', authMiddleware, async (req, res) => {
-    const schema = z.object({
-        gitUrl : z.string()
-    });
+async function deleteFolder(bucketName, folderId) {
+  const prefix = folderId.endsWith("/") ? folderId : folderId + "/";
+  let continuationToken = undefined;
 
-    const validation = schema.safeParse(req.body);
+  try {
+    do {
+      const listedObjects = await s3Client.send(
+        new ListObjectsV2Command({
+          Bucket: bucketName,
+          Prefix: prefix,
+          ContinuationToken: continuationToken,
+        })
+      );
 
-    if (!validation.success) {
-        return res.status(400).json({ error: 'Invalid input data' });
-    }
+      console.log("List of objects with prefix: ", listedObjects);
 
-    const { gitUrl } = validation.data;
-    const name = gitUrl.split('/')[4];
+      if (listedObjects.Contents && listedObjects.Contents.length > 0) {
+        const deleteParams = {
+          Bucket: bucketName,
+          Delete: {
+            Objects: listedObjects.Contents.map((obj) => ({ Key: obj.Key })),
+          },
+        }; 
+        await s3Client.send(new DeleteObjectsCommand(deleteParams));
+        console.log(`Deleted ${listedObjects.Contents.length} objects...`);
+      }
 
-    const existingProject = await prisma.project.findFirst({
-        where: { gitUrl }
-    });
+      continuationToken = listedObjects.NextContinuationToken;
+    } while (continuationToken);
+  } catch (err) {
+    console.error("Error deleting folder:", err);
+  }
+}
 
-    if (existingProject) {
-        return res.json({status: 'success', data: {project: existingProject}});
-    }
+router.post("/create", authMiddleware, async (req, res) => {
+  const schema = z.object({
+    gitUrl: z.string(),
+  });
 
-    const project = await prisma.project.create({
-        data: {
-            name,
-            gitUrl,
-            subDomain : generateSlug(),
-            userId: req.user.userId,
-        }
-    });
-    return res.json({status: 'success', data: {project}});
+  const validation = schema.safeParse(req.body);
+
+  if (!validation.success) {
+    return res.status(400).json({ error: "Invalid input data" });
+  }
+
+  const { gitUrl } = validation.data;
+  const name = gitUrl.split("/")[4];
+
+  const existingProject = await prisma.project.findFirst({
+    where: { gitUrl },
+  });
+
+  if (existingProject) {
+    return res.json({ status: "success", data: { project: existingProject } });
+  }
+
+  const project = await prisma.project.create({
+    data: {
+      name,
+      gitUrl,
+      subDomain: generateSlug(),
+      userId: req.user.userId,
+    },
+  });
+  return res.json({ status: "success", data: { project } });
 });
 
-router.post('/upload', authMiddleware, async (req, res) => {
-    const { projectId } = req.body;
+router.post("/upload", authMiddleware, async (req, res) => {
+  const { projectId } = req.body;
 
-    const project = await prisma.project.findUnique({
-        where: { id: projectId }
-    });
+  const project = await prisma.project.findUnique({
+    where: { id: projectId },
+  });
 
-    if (!project) {
-        return res.status(404).json({ error: 'Project not found' });
-    }
+  if (!project) {
+    return res.status(404).json({ error: "Project not found" });
+  }
 
-    //return error if project already has a deployment status QUEUEUED or BUILDING
-    const existingDeployment = await prisma.deployment.findFirst({
-        where: {
-            projectId: project.id,
-            OR: [
-                { status: 'QUEUED' },
-                { status: 'BUILDING' }
-            ]
-        }
-    });
-    if (existingDeployment) {
-        return res.status(400).json({ error: 'Project is already queued for build' });
-    }
+  //return error if project already has a deployment status QUEUEUED or BUILDING
+  const existingDeployment = await prisma.deployment.findFirst({
+    where: {
+      projectId: project.id,
+      OR: [{ status: "QUEUED" }, { status: "BUILDING" }],
+    },
+  });
+  if (existingDeployment) {
+    return res
+      .status(400)
+      .json({ error: "Project is already queued for build" });
+  }
 
-    const deployment = await prisma.deployment.create({
-        data: {
-            project : { connect: { id: project.id } },
-            status: 'QUEUED',
-        }
-    });
+  const deployment = await prisma.deployment.create({
+    data: {
+      project: { connect: { id: project.id } },
+      status: "QUEUED",
+    },
+  });
 
-    const command = new RunTaskCommand({
-        cluster: ecsConfig.cluster,
-        taskDefinition: ecsConfig.task,
-        launchType: 'FARGATE',
-        count: 1,
-        networkConfiguration: {
-            awsvpcConfiguration: {
-                subnets: ['subnet-056b58ac45618cfad', 'subnet-0a816b169578d08d8', 'subnet-03b3f6ebdb35a5b2b'],
-                securityGroups: ['sg-07e2ef174cb3b31e5'],
-                assignPublicIp: 'ENABLED'
-            }
+  const command = new RunTaskCommand({
+    cluster: ecsConfig.cluster,
+    taskDefinition: ecsConfig.task,
+    launchType: "FARGATE",
+    count: 1,
+    networkConfiguration: {
+      awsvpcConfiguration: {
+        subnets: [
+          "subnet-056b58ac45618cfad",
+          "subnet-0a816b169578d08d8",
+          "subnet-03b3f6ebdb35a5b2b",
+        ],
+        securityGroups: ["sg-07e2ef174cb3b31e5"],
+        assignPublicIp: "ENABLED",
+      },
+    },
+    overrides: {
+      containerOverrides: [
+        {
+          name: "builder-image",
+          environment: [
+            { name: "GIT_REPOSITORY_URL", value: project.gitUrl },
+            { name: "PROJECT_ID", value: projectId },
+            { name: "DEPLOYMENT_ID", value: deployment.id },
+          ],
         },
-        overrides: {
-            containerOverrides: [
-                {
-                    name: 'builder-image',
-                    environment: [
-                        { name: 'GIT_REPOSITORY_URL', value: project.gitUrl },
-                        { name: 'PROJECT_ID', value: projectId },
-                        { name: 'DEPLOYMENT_ID', value: deployment.id },
-                    ]
-                }
-            ]
-        }
-    });
+      ],
+    },
+  });
 
-    await ecsClient.send(command)
+  await ecsClient
+    .send(command)
     .then(() => {
-        res.json({
-            status: 'queued for build',
-            data: {
-                deploymentId: deployment.id
-            },
-        })
+      res.json({
+        status: "queued for build",
+        data: {
+          deploymentId: deployment.id,
+        },
+      });
     })
     .catch((error) => {
-        console.error('Error running ECS task:', error);
-        res.status(500).json({ error: 'Failed to start build process' });
+      console.error("Error running ECS task:", error);
+      res.status(500).json({ error: "Failed to start build process" });
     });
 });
 
-router.get('/', authMiddleware, async (req, res) => {
-    const projects = await prisma.project.findMany({
-        where: { 
-            userId: req.user.userId
-        },
-        orderBy: { createdAt: 'desc' }
-    });
-    
-    res.json({ status: 'success', data: { projects } });
+router.get("/", authMiddleware, async (req, res) => {
+  const projects = await prisma.project.findMany({
+    where: {
+      userId: req.user.userId,
+    },
+    orderBy: { createdAt: "desc" },
+  });
+
+  res.json({ status: "success", data: { projects } });
 });
 
-router.get('/:projectId/deployments', authMiddleware, async (req, res) => {
-    const { projectId } = req.params;
+router.get("/:projectId/deployments", authMiddleware, async (req, res) => {
+  const { projectId } = req.params;
 
-    const project = await prisma.project.findUnique({
-        where: { id: projectId }
-    });
+  const project = await prisma.project.findUnique({
+    where: { id: projectId },
+  });
 
-    if (!project) {
-        return res.status(404).json({ error: 'Project not found' });
-    }
+  if (!project) {
+    return res.status(404).json({ error: "Project not found" });
+  }
 
+  const deployments = await prisma.deployment.findMany({
+    where: { projectId: project.id },
+    orderBy: { createdAt: "desc" },
+  });
+
+  res.json({ status: "success", data: { deployments } });
+});
+
+router.delete("/:projectId", authMiddleware, async (req, res) => {
+  const { projectId } = req.params;
+
+  try {
     const deployments = await prisma.deployment.findMany({
-        where: { projectId: project.id },
-        orderBy: { createdAt: 'desc' }
+      where: {
+        projectId: projectId,
+      },
+      select: {
+        id: true,
+      },
     });
-
-    res.json({ status: 'success', data: { deployments } });
-});
-
-router.delete('/:projectId', authMiddleware, async (req, res) => {
-    const { projectId } = req.params;
-
-    const deployments = await prisma.deployment.findMany({
-        where: {
-            projectId: projectId
-        },
-        select: {
-            id: true
-        }
-    });
-    const deploymentIds = deployments.map(deployment => deployment.id);
+    const deploymentIds = deployments.map((deployment) => deployment.id);
     // Delete associated log events from ClickHouse
     if (deploymentIds.length > 0) {
-        const deleteQuery = `ALTER TABLE log_events DELETE WHERE deployment_id IN ({deployment_ids:Array(String)})`;
-        await client.query({
-            query: deleteQuery,
-            query_params: {
-                deployment_ids: deploymentIds
-            }
-        });
+      const deleteQuery = `ALTER TABLE log_events DELETE WHERE deployment_id IN ({deployment_ids:Array(String)})`;
+      await client.query({
+        query: deleteQuery,
+        query_params: {
+          deployment_ids: deploymentIds,
+        },
+      });
     }
 
     await prisma.deployment.deleteMany({
-        where: { projectId: projectId }
+      where: { projectId: projectId },
     });
 
     await prisma.project.delete({
-        where: { id: projectId }
+      where: { id: projectId },
     });
 
-    //#TODO: Delete AWS S3 folder for the project
+    //delete project files from AWS S3
+    const projectDirectory = `__outputs/${projectId}`;
+    await deleteFolder(process.env.S3_BUCKET, projectDirectory);
 
-    res.json({ status: 'success', message: 'Project deleted successfully' });
+    res.json({ status: "success", message: "Project deleted successfully" });
+  } catch (error) {
+    console.log("Error deleting project: ", error);
+  }
 });
 
-router.get('/analytics', authMiddleware, async (req, res) => {
-    const projects = await prisma.project.findMany({
-        where: {
-            userId: req.user.userId
-        },
-        include: {
-            Analytics: true
-        }
-    });
-    res.status(200).json({projects: projects});
+router.get("/analytics", authMiddleware, async (req, res) => {
+  const projects = await prisma.project.findMany({
+    where: {
+      userId: req.user.userId,
+    },
+    include: {
+      Analytics: true,
+    },
+  });
+  res.status(200).json({ projects: projects });
 });
 
-router.get('/visits/:projectId', authMiddleware, async (req,res) => {
-    const {projectId} = req.params;
+router.get("/visits/:projectId", authMiddleware, async (req, res) => {
+  const { projectId } = req.params;
 
-    const visits = await prisma.analytic.count({
-        where:{
-            projectId : projectId
-        }
-    });
-    res.status(200).json({visits: visits});
+  const visits = await prisma.analytic.count({
+    where: {
+      projectId: projectId,
+    },
+  });
+  res.status(200).json({ visits: visits });
 });
 
-router.post('/analytics/:projectId', authMiddleware, async (req, res) => {
-    const { projectId } = req.params;
-    console.log(req);
-    const startDate = req.body.startDateString;
-    const endDate = req.body.endDateString;
+router.post("/analytics/:projectId", authMiddleware, async (req, res) => {
+  const { projectId } = req.params;
+  console.log(req);
+  const startDate = req.body.startDateString;
+  const endDate = req.body.endDateString;
 
-    const results = await prisma.$queryRaw`
+  const results = await prisma.$queryRaw`
     SELECT DATE("timestamp") as date, COUNT(*) as count
     FROM "Analytic"
     WHERE "projectId" = ${projectId}
@@ -245,23 +302,23 @@ router.post('/analytics/:projectId', authMiddleware, async (req, res) => {
     GROUP BY DATE("timestamp")
     ORDER BY date;
     `;
-    const normalizedRes = results.map(r => ({
-        date: r.date.toLocaleDateString('en-CA'),
-        visits: Number(r.count)
-    }));
+  const normalizedRes = results.map((r) => ({
+    date: r.date.toLocaleDateString("en-CA"),
+    visits: Number(r.count),
+  }));
 
-    console.log("anlaytics: ", normalizedRes);
-    res.json(normalizedRes);
+  console.log("anlaytics: ", normalizedRes);
+  res.json(normalizedRes);
 });
 
-router.get('/:projectId', authMiddleware, async (req, res) => {
-    const { projectId } = req.params;
-    const project = await prisma.project.findFirst({
-        where: {
-            id: projectId
-        }
-    });
-    res.status(200).json({project: project});
+router.get("/:projectId", authMiddleware, async (req, res) => {
+  const { projectId } = req.params;
+  const project = await prisma.project.findFirst({
+    where: {
+      id: projectId,
+    },
+  });
+  res.status(200).json({ project: project });
 });
 
 module.exports = router;
